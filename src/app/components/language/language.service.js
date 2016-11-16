f9d3e2ca9c25deb6f8066cbdb5ff361552c68fa7
Axel <axel.bie@viacesi.fr>
2016-11-16 12:02:20 +0100

    Filled language.service

--- src/app/components/language/language.service.js
+++ src/app/components/language/language.service.js
@@ -1,3 +1,39 @@
 /**
  * Created by Axel on 16/11/2016.
  */
+(function () {
+    'use strict';
+
+    angular
+        .module('dataToolApp')
+        .service('DtoolLanguageService', DtoolLanguageService);
+
+    DtoolLanguageService.$inject = ['$q', '$http', '$translate', 'LANGUAGES'];
+
+    /* @ngInject */
+    function DtoolLanguageService($q, $http, $translate, LANGUAGES) {
+        var service = {
+            getAll: getAll,
+            getCurrent: getCurrent
+        };
+
+        return service;
+
+        function getAll () {
+            var deferred = $q.defer();
+            deferred.resolve(LANGUAGES);
+            return deferred.promise;
+        }
+
+        function getCurrent () {
+            var deferred = $q.defer();
+            var language = $translate.storage().get('NG_TRANSLATE_LANG_KEY');
+
+            deferred.resolve(language);
+
+            return deferred.promise;
+        }
+    }
+
+})();
+
