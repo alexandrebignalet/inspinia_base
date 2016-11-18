f342b6be04ba86306ec95aadbffd40d89af9ca25
TestGitName <qwe@qwe.fr>
2016-11-18 11:31:22 +0100

    Added Announcer factory. Added base files for platform access

---
+++ src/app/components/announcer/announcer.service.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Axel on 18/11/2016.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('dataToolApp')
+        .factory('Announcer', Announcer);
+
+    Announcer.$inject = ['$resource','API_BASE_URL'];
+
+    /* @ngInject */
+    function  Announcer($resource, API_BASE_URL) {
+        var resourceUrl = API_BASE_URL+'/announcers/:id';
+
+        return $resource(resourceUrl, {}, {
+            'update': {method: 'PUT'},
+            'save_platform_access': {
+                method: 'POST',
+                url:  API_BASE_URL+'/announcers/:announcerId/platform-access/json'
+            },
+            'update_platform_access': {
+                method: 'PATCH',
+                url:  API_BASE_URL+'/announcers/platform-access/:platformId/json',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            },
+            'delete_platform_access' : {
+                method: 'DELETE',
+                url:  API_BASE_URL+'/announcers/platform-access/:platformId/json'
+            }
+        });
+    }
+
+})();
+
