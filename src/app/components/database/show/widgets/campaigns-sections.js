8eb27fd3af410ba3f575545b4ab6b352275bb0ef
AxelBie <axel.bie@viacesi.fr>
2016-12-07 17:34:17 +0100

    Widget avg volume

---
+++ src/app/components/database/show/widgets/campaigns-sections.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    var campaignsSections = {
+        templateUrl: 'app/components/database/show/widgets/campaigns-sections.html',
+        controller: CampaignsSectionsController,
+        controllerAs: 'vm',
+        bindings: {
+            ladderTag: '<',
+            ladderCampaign: '<',
+            incomesByBm: '<'
+        }
+    };
+
+    angular
+        .module('dataToolApp')
+        .component('campaignsSections', campaignsSections);
+
+    CampaignsSectionsController.$inject = ['Database'];
+
+    /* @ngInject */
+    function CampaignsSectionsController() {
+        var vm = this;
+
+        vm.ecpmMax = 0.5;
+        vm.ecpmMin = 0.2;
+
+
+
+    }
+
+})();
