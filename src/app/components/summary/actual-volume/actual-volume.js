dd44436dd9ed0e4d4f2fb50bc10cf7219047c094
AxelBie <axel.bie@viacesi.fr>
2016-12-13 18:31:49 +0100

    La ca marche mais c'est un peu magic

--- src/app/components/summary/actual-volume/actual-volume.js
+++ src/app/components/summary/actual-volume/actual-volume.js
@@ -21,15 +21,33 @@
         vm.dates = {};
         vm.stats = [];       // RECEIVED DATA
         vm.databases = [];   // TAB DATA
-        vm.dataFilters = {}; // FILTER CONTENT
-        vm.totals = {};
+        vm.test = [];
+        vm.totals = {
+            actualVolume: 0,
+            daybeforeVolume: 0,
+            predictActualVolume: 0,
+            predictDaybeforeVolume: 0,
+            predictCombined: 0
+        };
+        vm.chartVolumeObject = {
+            labels: [],
+            data: [],
+            series: ['Volumes','Average']
+        };
+        vm.chartTotalsObject = {
+            labels: [],
+            data: [],
+            series: ['TotalsAvg','TotalsDayBefore']
+        };
 
+        vm.onChangeFilters = onChangeFilters;
         vm.onChangeDates = onChangeDates;
         vm.$onInit = onInit;
 
         /////////////////////////////////////////////
 
         function onInit(){
+            vm.filtersContent = {foo: 'foo' };
         }
 
         function onChangeDates($event)
@@ -47,8 +65,22 @@
                 .catch(onError);
 
             function onSuccess(response){
+
                 vm.stats = response.data;
-                parseData();
+                vm.stats.sort(function(a,b){
+                    var dateA = moment(a.date.date);
+                    var dateB = moment(b.date.date);
+
+                    return dateA - dateB;
+                });
+
+
+                vm.filtersContent = ActualVolume.parseResponse(vm.stats,vm.databases,vm.filtersContent);
+
+                console.log(vm.filtersContent);
+
+                ActualVolume.getProcessedValues(vm.databases,vm.totals, vm.dates.endDateDay);
+                ActualVolume.getGraphsObject(vm.stats, vm.chartVolumeObject, vm.chartTotalsObject);
             }
 
             function onError(error) {
@@ -56,332 +88,11 @@
             }
         }
 
-        function parseData() {
+        function onChangeFilters($event) {
 
-            vm.stats.sort(function(a,b){
-                var dateA = moment(a.date.date);
-                var dateB = moment(b.date.date);
+            vm.filtersValue = $event.filters;
+            console.log(vm.filtersValue);
 
-                return dateA - dateB;
-            });
-
-            loadDataForVolumesChart();
-
-            angular.forEach(vm.stats, function(stat) {
-
-                addStatToDatabase(stat);
-
-                addCompanyToFilter(stat);
-                addCountryToFilter(stat);
-                addTypeToFilter(stat);
-
-
-            });
-
-            angular.forEach(vm.databases, function(database) {
-                setProcessedValues(database);
-            });
-        }
-
-        /*------------------------------------//
-
-             PROCESSING DATA FOR GRAPH
-
-             IN : DATABSES SELECTED
-             OUT : GRAPH OBJECTS
-
-         //------------------------------------*/
-
-
-        vm.chartVolumeObject = {
-            labels: [],
-            data: [],
-            series: ['Volumes','Average']
-        };
-
-        vm.chartTotalsObject = {
-            labels: [],
-            data: [],
-            series: ['TotalsAvg','TotalsDayBefore']
-        };
-
-        /**
-         * IN : Ordered stats by date ASC ( Mandatory for function to work properly )
-         */
-        function loadDataForVolumesChart() {
-
-            var dateFormat = 'YYYY-MM-DD';
-            var labelsVolume     = [];
-            var labelsTotals     = [];
-            var dataVolume = [];
-            var dataTotals = [];
-            var total      = 0;
-            var dataIndex  = 0;
-            var statsLength = vm.stats.length;
-
-
-            /**
-             * Init of labels and data with the first stat
-             */
-            var firstDate = moment(vm.stats[0].date.date).format(dateFormat);
-            var firstValue = +vm.stats[0].volume_sent;
-
-            labelsVolume.push(firstDate);
-            dataVolume[dataIndex] = firstValue;
-
-            total += firstValue;
-
-            /**
-             * Loop to insert the data and labels starting to index 1
-             */
-            var currentDate = firstDate;
-            var stat;
-            var statValue;
-            var statDate;
-
-            for( var i = 1 ; i < statsLength ; i++) {
-
-                stat = vm.stats[i];
-                statDate = moment(stat.date.date).format(dateFormat);
-                statValue = +stat.volume_sent;
-                total += statValue;
-
-                if(statDate == currentDate) {
-                    dataVolume[dataIndex] += statValue;
-                } else {
-                    labelsVolume.push(statDate);
-                    currentDate = statDate;
-                    dataTotals.push(total);
-                    dataIndex++;
-                    dataVolume[dataIndex] = statValue
-                }
-            }
-
-            dataTotals.push(total);
-
-            /**
-             * Calcul des prévisions
-             */
-            var dataTotalsAvg = angular.copy(dataTotals);
-            var dataTotalsDayBefore = angular.copy(dataTotals);
-            var dataTotalsLength = dataTotals.length;
-            var lastTotalAvg = dataTotals[dataTotalsLength - 1];
-            var lastTotalDayBefore = dataTotals[dataTotalsLength - 1];
-            var stepAvg = +( lastTotalAvg / dataTotalsLength ).toFixed(0);
-            var daysInMonth = moment(currentDate).endOf('month').format('DD');
-            var daysLeft = daysInMonth - dataTotalsLength;
-
-            var stepDayBefore = +(dataTotals[dataTotalsLength - 1] - dataTotals[dataTotalsLength - 2]);
-
-            labelsTotals = angular.copy(labelsVolume);
-
-            for( var k = daysLeft ; k > 0 ; k-- ) {
-                currentDate = moment(currentDate).add(1, 'days');
-
-                lastTotalAvg += stepAvg;
-                lastTotalDayBefore += stepDayBefore;
-
-                labelsTotals.push(currentDate.format(dateFormat));
-
-                dataTotalsAvg.push(lastTotalAvg);
-                dataTotalsDayBefore.push(lastTotalDayBefore);
-            }
-
-            /**
-             * Calcul de la moyenne
-             */
-            var labelsLength = labelsVolume.length;
-            var avg = ( total / labelsLength ).toFixed(0);
-            var dataAvg = [];
-
-            for( var j = 0 ; j < labelsLength ; j++ ){
-                dataAvg[j] = avg;
-            }
-
-            vm.chartVolumeObject.labels = labelsVolume;
-            vm.chartVolumeObject.data.push(dataVolume);
-            vm.chartVolumeObject.data.push(dataAvg);
-
-            vm.chartTotalsObject.labels = labelsTotals;
-            vm.chartTotalsObject.data.push(dataTotalsAvg);
-            vm.chartTotalsObject.data.push(dataTotalsDayBefore);
-        }
-
-
-        /*------------------------------------//
-
-            PROCESSING STATS SECTION FOR TAB
-
-            IN : DATABASES SELECTED
-            OUT : TOTALS (table),
-
-         //------------------------------------*/
-
-        vm.totals = {
-            actualVolume: 0,
-            daybeforeVolume: 0,
-            predictActualVolume: 0,
-            predictDaybeforeVolume: 0,
-            predictCombined: 0
-        };
-
-        function setProcessedValues(database) {
-
-            var predictActualVolumeDayBefore = getPredictVolumeDayBefore(database.daybefore_volume, database.date, database.total);
-            var predictVolumeDayBefore = getPredictVolumeAsMonth(database.daybefore_volume, database.date);
-            var predictVolume = getPredictActualVolume(database.total, database.date);
-
-            database.predictActualVolumeDayBefore = predictActualVolumeDayBefore;
-            database.predictVolumeDayBefore       = predictVolumeDayBefore;
-            database.predictVolume                = predictVolume;
-
-            vm.totals.predictDaybeforeVolume += predictVolumeDayBefore;
-            vm.totals.predictCombined += predictActualVolumeDayBefore;
-            vm.totals.predictActualVolume += predictVolume;
-            vm.totals.actualVolume += database.total;
-            vm.totals.daybeforeVolume += database.daybefore_volume;
-        }
-
-        function getPredictVolumeDayBefore(volume, date, total) {
-
-            var dayBeforeV;
-            var remainingDays;
-            var daysInMonth = +moment(date).endOf('month').format('DD');
-
-            if (vm.dates.endDateDay < moment().format('DD')) {
-                dayBeforeV = vm.dates.endDateDay - 1;
-            } else {
-                //noinspection JSUnresolvedFunction
-                dayBeforeV = moment().subtract(1, 'day').format('DD');
-            }
-
-            remainingDays = daysInMonth - dayBeforeV;
-
-            return (remainingDays * +volume) + total;
-        }
-
-        function getPredictActualVolume(volume, date) {
-
-            var pastDays;
-            var daysInMonth;
-            var averageDailyVolume;
-
-            if (vm.dates.endDateDay < moment().format('DD')) {
-                pastDays = vm.dates.endDateDay;
-            } else {
-                pastDays = moment().format('DD');
-            }
-
-            daysInMonth = moment(date).endOf('month').format('DD');
-            averageDailyVolume = +volume / pastDays;
-
-            return averageDailyVolume * daysInMonth;
-        }
-
-        function getPredictVolumeAsMonth(volume, date) {
-
-            var daysInMonth = moment(date).endOf('month').format('DD');
-
-            return +volume * daysInMonth;
-        }
-
-        /*------------------------------------//
-
-             PROCESSING STATS SECTION FOR FORMATING
-
-             IN : STATS (from HTTP Request)
-             OUT : DATABASES + FILTERS CONTENT
-
-         //------------------------------------*/
-
-        vm.dataFilters = {
-            databases: [],
-            companies: [],
-            types:     [],
-            countries:   [],
-            actives: [{
-                name: 'Enabled',
-                value: true
-            },{
-                name: 'Disabled',
-                value: false
-            }]
-        };
-
-        function addStatToDatabase(value) {
-
-            var index = indexOfArrayObject(value.database_id, vm.databases, 'id');
-
-            if( index == -1){
-                var databaseStat = {
-                    summary_id: value.resume_id,
-                    id: value.database_id,
-                    company_id: value.company_id,
-                    company_name: value.company_name,
-                    database_type: value.database_type,
-                    active: value.active,
-                    planche_id: value.planche_id,
-                    router_name: value.router_name,
-                    database_name: value.database_name,
-                    database_country: value.database_country,
-                    total: +value.volume_sent,
-                    date: value.date.date,
-                    daybefore_volume: +value.volume_sent
-                };
-
-                var databaseFilter = {
-                    id: value.database_id,
-                    name: value.database_name
-                };
-
-                vm.dataFilters.databases.push(databaseFilter);
-                vm.databases.push(databaseStat);
-
-            } else {
-                vm.dataFilters.databases[index].total += value.volume_sent;
-                if( value.date.date >vm.dataFilters.databases[index].date ) {
-                    vm.dataFilters.databases[index] = value.date.date;
-                    vm.dataFilters.databases[index].daybefore_volume = value.volume_sent;
-                }
-            }
-
-        }
-
-        function addCompanyToFilter(stat) {
-
-            if( indexOfArrayObject(stat.company_id, vm.dataFilters.companies, 'id') == -1) {
-                var company = {
-                    id: stat.company_id,
-                    name: stat.company_name
-                };
-                vm.dataFilters.companies.push(company);
-            }
-        }
-
-        function addCountryToFilter(stat) {
-
-            if( indexOfArrayObject(stat.company_country, vm.dataFilters.countries, 'name') == -1) {
-                var country = {
-                    name: stat.company_country
-                };
-                vm.dataFilters.countries.push(country);
-            }
-        }
-
-        function addTypeToFilter(stat) {
-            if( indexOfArrayObject(stat.database_type, vm.dataFilters.types, 'name') == -1) {
-                var type = {
-                    name: stat.database_type
-                };
-                vm.dataFilters.types.push(type);
-            }
-        }
-
-        function indexOfArrayObject(value,array,field) {
-            for( var res = 0 ; res < array.length ; res++ ) {
-                if (array[res][field] == value) { return res }
-            }
-            return -1;
         }
 
     }
--- src/app/components/summary/actual-volume/actual-volume.service.js
+++ src/app/components/summary/actual-volume/actual-volume.service.js
@@ -18,7 +18,10 @@
         var service = {
             getSummaryVolumes: getSummaryVolumes,
             getCommentByDate: getCommentByDate,
-            saveComment: saveComment
+            saveComment: saveComment,
+            parseResponse: parseResponse,
+            getProcessedValues: getProcessedValues,
+            getGraphsObject: getGraphsObject
         };
 
         return service;
@@ -73,72 +76,108 @@
 
          /----------------------------------------------*/
 
-        function addStatToDatabase(value) {
+        function parseResponse(data,databases,filters) {
 
-            var index = indexOfArrayObject(value.database_id, vm.databases, 'id');
+            var tmp = {
+                databases: [],
+                companies: [],
+                types: [],
+                countries: [],
+                actives: [{
+                    name: 'Enabled',
+                    value: true
+                }, {
+                    name: 'Disabled',
+                    value: false
+                }]
+            };
+
+
+            angular.forEach(data, function(stat) {
+
+                addStatToDatabases(stat,databases,tmp);
+
+                addCompanyToFilter(stat,tmp);
+                addCountryToFilter(stat,tmp);
+                addTypeToFilter(stat,tmp);
+            });
+
+            return tmp;
+        }
+
+        function addStatToDatabases(value,databases,filters) {
+
+            var index = indexOfArrayObject(value.database_id, databases, 'id');
 
             if( index == -1){
-                var databaseStat = {
-                    summary_id: value.resume_id,
-                    id: value.database_id,
-                    company_id: value.company_id,
-                    company_name: value.company_name,
-                    database_type: value.database_type,
-                    active: value.active,
-                    planche_id: value.planche_id,
-                    router_name: value.router_name,
-                    database_name: value.database_name,
-                    database_country: value.database_country,
-                    total: +value.volume_sent,
-                    date: value.date.date,
-                    daybefore_volume: +value.volume_sent
-                };
+
+                var databaseStat = initDatabaseStat(value);
 
                 var databaseFilter = {
                     id: value.database_id,
                     name: value.database_name
                 };
 
-                vm.dataFilters.databases.push(databaseFilter);
-                vm.databases.push(databaseStat);
+                filters.databases.push(databaseFilter);
+                databases.push(databaseStat);
 
             } else {
-                vm.dataFilters.databases[index].total += value.volume_sent;
-                if( value.date.date >vm.dataFilters.databases[index].date ) {
-                    vm.dataFilters.databases[index] = value.date.date;
-                    vm.dataFilters.databases[index].daybefore_volume = value.volume_sent;
+                databases[index].total += +value.volume_sent;
+                if( value.date.date > databases[index].date ) {
+                    databases[index].date = value.date.date;
+                    databases[index].daybefore_volume = +value.volume_sent;
                 }
             }
-
         }
 
-        function addCompanyToFilter(stat) {
+        function initDatabaseStat(value) {
 
-            if( indexOfArrayObject(stat.company_id, vm.dataFilters.companies, 'id') == -1) {
+            var databaseStat = {
+                summary_id: value.resume_id,
+                id: value.database_id,
+                company_id: value.company_id,
+                company_name: value.company_name,
+                database_type: value.database_type,
+                active: value.active,
+                planche_id: value.planche_id,
+                router_name: value.router_name,
+                database_name: value.database_name,
+                database_country: value.database_country,
+                total: +value.volume_sent,
+                date: value.date.date,
+                daybefore_volume: +value.volume_sent
+            };
+
+            return databaseStat;
+        }
+
+        function addCompanyToFilter(stat,filters) {
+
+            if( indexOfArrayObject(stat.company_id, filters.companies, 'id') == -1) {
                 var company = {
                     id: stat.company_id,
                     name: stat.company_name
                 };
-                vm.dataFilters.companies.push(company);
+                filters.companies.push(company);
             }
         }
 
-        function addCountryToFilter(stat) {
+        function addCountryToFilter(stat,filters) {
 
-            if( indexOfArrayObject(stat.company_country, vm.dataFilters.countries, 'name') == -1) {
+            if( indexOfArrayObject(stat.company_country, filters.countries, 'name') == -1) {
                 var country = {
                     name: stat.company_country
                 };
-                vm.dataFilters.countries.push(country);
+                filters.countries.push(country);
             }
         }
 
-        function addTypeToFilter(stat) {
-            if( indexOfArrayObject(stat.database_type, vm.dataFilters.types, 'name') == -1) {
+        function addTypeToFilter(stat,filters) {
+            if( indexOfArrayObject(stat.database_type, filters.types, 'name') == -1) {
                 var type = {
                     name: stat.database_type
                 };
-                vm.dataFilters.types.push(type);
+                filters.types.push(type);
             }
         }
 
@@ -148,6 +187,190 @@
             }
             return -1;
         }
+
+        /*------------------------------------//
+
+         PROCESSING STATS SECTION FOR TAB
+
+         IN : DATABASES FILTERED
+         OUT : TOTALS (table),
+
+         //------------------------------------*/
+
+        function getProcessedValues(databases, totals, endDateDay) {
+            angular.forEach(databases, function(database) {
+                setProcessedValues(database,totals,endDateDay);
+            });
+        }
+
+        function setProcessedValues(database,totals,endDateDay) {
+
+            var predictActualVolumeDayBefore = getPredictVolumeDayBefore(database.daybefore_volume, database.date, database.total);
+            var predictVolumeDayBefore = getPredictVolumeAsMonth(database.daybefore_volume, database.date);
+            var predictVolume = getPredictActualVolume(database.total, database.date);
+
+            database.predictActualVolumeDayBefore = predictActualVolumeDayBefore;
+            database.predictVolumeDayBefore       = predictVolumeDayBefore;
+            database.predictVolume                = predictVolume;
+
+            totals.predictDaybeforeVolume += predictVolumeDayBefore;
+            totals.predictCombined += predictActualVolumeDayBefore;
+            totals.predictActualVolume += predictVolume;
+            totals.actualVolume += database.total;
+            totals.daybeforeVolume += database.daybefore_volume;
+        }
+
+        function getPredictVolumeDayBefore(volume, date, total, endDateDay) {
+
+            var dayBeforeV;
+            var remainingDays;
+            var daysInMonth = +moment(date).endOf('month').format('DD');
+
+            if (endDateDay < moment().format('DD')) {
+                dayBeforeV = endDateDay - 1;
+            } else {
+                //noinspection JSUnresolvedFunction
+                dayBeforeV = moment().subtract(1, 'day').format('DD');
+            }
+
+            remainingDays = daysInMonth - dayBeforeV;
+
+            return (remainingDays * +volume) + total;
+        }
+
+        function getPredictActualVolume(volume, date, endDateDay) {
+
+            var pastDays;
+            var daysInMonth;
+            var averageDailyVolume;
+
+            if (endDateDay < moment().format('DD')) {
+                pastDays = endDateDay;
+            } else {
+                pastDays = moment().format('DD');
+            }
+
+            daysInMonth = moment(date).endOf('month').format('DD');
+            averageDailyVolume = +volume / pastDays;
+
+            return averageDailyVolume * daysInMonth;
+        }
+
+        function getPredictVolumeAsMonth(volume, date) {
+
+            var daysInMonth = moment(date).endOf('month').format('DD');
+
+            return +volume * daysInMonth;
+        }
+
+        /*------------------------------------//
+
+         PROCESSING DATA FOR GRAPH
+
+         IN : DATABSES SELECTED
+         OUT : GRAPH OBJECTS
+
+         //------------------------------------*/
+
+        function getGraphsObject(stats,chartVolume,chartTotals) {
+
+            var dateFormat = 'YYYY-MM-DD';
+            var labelsVolume     = [];
+            var labelsTotals     = [];
+            var dataVolume = [];
+            var dataTotals = [];
+            var total      = 0;
+            var dataIndex  = 0;
+            var statsLength = stats.length;
+
+
+            /**
+             * Init of labels and data with the first stat
+             */
+            var firstDate = moment(stats[0].date.date).format(dateFormat);
+            var firstValue = +stats[0].volume_sent;
+
+            labelsVolume.push(firstDate);
+            dataVolume[dataIndex] = firstValue;
+
+            total += firstValue;
+
+            /**
+             * Loop to insert the data and labels starting to index 1
+             */
+            var currentDate = firstDate;
+            var stat;
+            var statValue;
+            var statDate;
+
+            for( var i = 1 ; i < statsLength ; i++) {
+
+                stat = stats[i];
+                statDate = moment(stat.date.date).format(dateFormat);
+                statValue = +stat.volume_sent;
+                total += statValue;
+
+                if(statDate == currentDate) {
+                    dataVolume[dataIndex] += statValue;
+                } else {
+                    labelsVolume.push(statDate);
+                    currentDate = statDate;
+                    dataTotals.push(total);
+                    dataIndex++;
+                    dataVolume[dataIndex] = statValue
+                }
+            }
+
+            dataTotals.push(total);
+
+            /**
+             * Calcul des prévisions
+             */
+            var dataTotalsAvg = angular.copy(dataTotals);
+            var dataTotalsDayBefore = angular.copy(dataTotals);
+            var dataTotalsLength = dataTotals.length;
+            var lastTotalAvg = dataTotals[dataTotalsLength - 1];
+            var lastTotalDayBefore = dataTotals[dataTotalsLength - 1];
+            var stepAvg = +( lastTotalAvg / dataTotalsLength ).toFixed(0);
+            var daysInMonth = moment(currentDate).endOf('month').format('DD');
+            var daysLeft = daysInMonth - dataTotalsLength;
+
+            var stepDayBefore = +(dataTotals[dataTotalsLength - 1] - dataTotals[dataTotalsLength - 2]);
+
+            labelsTotals = angular.copy(labelsVolume);
+
+            for( var k = daysLeft ; k >= 0 ; k-- ) {
+                currentDate = moment(currentDate).add(1, 'days');
+
+                lastTotalAvg += stepAvg;
+                lastTotalDayBefore += stepDayBefore;
+
+                labelsTotals.push(currentDate.format(dateFormat));
+
+                dataTotalsAvg.push(lastTotalAvg);
+                dataTotalsDayBefore.push(lastTotalDayBefore);
+            }
+
+            /**
+             * Calcul de la moyenne
+             */
+            var labelsLength = labelsVolume.length;
+            var avg = ( total / labelsLength ).toFixed(0);
+            var dataAvg = [];
+
+            for( var j = 0 ; j < labelsLength ; j++ ){
+                dataAvg[j] = avg;
+            }
+
+            chartVolume.labels = labelsVolume;
+            chartVolume.data.push(dataVolume);
+            chartVolume.data.push(dataAvg);
+
+            chartTotals.labels = labelsTotals;
+            chartTotals.data.push(dataTotalsAvg);
+            chartTotals.data.push(dataTotalsDayBefore);
+        }
+
 
     }
 
